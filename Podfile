f11b51002a881d89640a3270ecfae11001b5645f
Weiran Zhang <weiran@zhang.me.uk>
2013-01-14 15:01:18 +0000

    Added DTCoreText library in preparation for moving to it.

--- Podfile
+++ Podfile
@@ -1,3 +1,4 @@
 platform :ios, '6.0'
 pod 'AFNetworking'
 pod 'SWRevealViewController'
+pod 'DTCoreText', :podspec => 'https://raw.github.com/Cocoanetics/DTCoreText/master/DTCoreText.podspec'
---
+++ Pods/DTCoreText/Core/Source/NSCharacterSet+HTML.m
@@ -0,0 +1,97 @@
+//
+//  NSCharacterSet+HTML.m
+//  CoreTextExtensions
+//
+//  Created by Oliver Drobnik on 1/15/11.
+//  Copyright 2011 Drobnik.com. All rights reserved.
+//
+
+#import "NSCharacterSet+HTML.h"
+#import "DTCoreTextConstants.h"
+
+static NSCharacterSet *_tagNameCharacterSet = nil;
+static NSCharacterSet *_ignorableWhitespaceCharacterSet = nil;
+static NSCharacterSet *_tagAttributeNameCharacterSet = nil;
+static NSCharacterSet *_quoteCharacterSet = nil;
+static NSCharacterSet *_nonQuotedAttributeEndCharacterSet = nil;
+static NSCharacterSet *_cssStyleAttributeNameCharacterSet = nil;
+
+
+@implementation NSCharacterSet (HTML)
+
++ (NSCharacterSet *)tagNameCharacterSet
+{
+	static dispatch_once_t predicate;
+
+	dispatch_once(&predicate, ^{
+		_tagNameCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"];
+	});
+	
+	return _tagNameCharacterSet;
+}
+
++ (NSCharacterSet *)tagAttributeNameCharacterSet
+{
+	static dispatch_once_t predicate;
+
+	dispatch_once(&predicate, ^{
+		_tagAttributeNameCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"-_:abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"];
+	});
+	
+	return _tagAttributeNameCharacterSet;
+}
+
++ (NSCharacterSet *)ignorableWhitespaceCharacterSet
+{
+	static dispatch_once_t predicate;
+	
+	dispatch_once(&predicate, ^{
+		NSMutableCharacterSet *tmpSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+		[tmpSet removeCharactersInString:UNICODE_NON_BREAKING_SPACE];
+		
+		_ignorableWhitespaceCharacterSet = [tmpSet copy];
+	});
+	
+	return _ignorableWhitespaceCharacterSet;
+}
+
+
++ (NSCharacterSet *)quoteCharacterSet
+{
+	static dispatch_once_t predicate;
+
+	dispatch_once(&predicate, ^{
+		_quoteCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"'\""];
+	});
+	
+	return _quoteCharacterSet;
+}
+
++ (NSCharacterSet *)nonQuotedAttributeEndCharacterSet
+{
+	static dispatch_once_t predicate;
+
+	dispatch_once(&predicate, ^{
+		NSMutableCharacterSet *tmpCharacterSet = [NSMutableCharacterSet characterSetWithCharactersInString:@"/>"];
+		[tmpCharacterSet formUnionWithCharacterSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+		
+		_nonQuotedAttributeEndCharacterSet = [tmpCharacterSet copy];
+	});
+	
+	return _nonQuotedAttributeEndCharacterSet;
+}
+
+// NOTE: cannot contain : because otherwise this messes up parsing of CSS style attributes
++ (NSCharacterSet *)cssStyleAttributeNameCharacterSet
+{
+	static dispatch_once_t predicate;
+
+	dispatch_once(&predicate, ^{
+		_cssStyleAttributeNameCharacterSet = [NSCharacterSet characterSetWithCharactersInString:@"-_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"];
+	});	
+	return _cssStyleAttributeNameCharacterSet;
+}
+
+
+
+@end
