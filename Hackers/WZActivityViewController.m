31e3cb7c3a0a31cc8e35990d82c6bd0210138eb7
Weiran Zhang <weiran@zhang.me.uk>
2013-03-01 12:17:58 +0000

    Now tapping outside the bounds of the activity action sheet closes itself.

--- Hackers/WZActivityViewController.m
+++ Hackers/WZActivityViewController.m
@@ -10,24 +10,38 @@
 
 #import <TUSafariActivity/TUSafariActivity.h>
 
+@interface WZActivityViewController () {
+    UITapGestureRecognizer *_tapGestureRecognizer;
+}
+@end
+
 @implementation WZActivityViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // gesture recognizer to dismiss UIActivityView when tapped outside
-    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
-                                                                                           action:@selector(tapOut:)];
-    tapGestureRecognizer.cancelsTouchesInView = NO;
-    [self.view addGestureRecognizer:tapGestureRecognizer];
+    _tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                    action:@selector(tapOut:)];
+    _tapGestureRecognizer.cancelsTouchesInView = NO;
+    [self.view addGestureRecognizer:_tapGestureRecognizer];
+}
+
+- (void)viewWillUnload {
+    [self.view removeGestureRecognizer:_tapGestureRecognizer];
 }
 
 - (void)tapOut:(id)sender {
     UITapGestureRecognizer *tapGestureRecognizer = (UITapGestureRecognizer *)sender;
     CGPoint point = [tapGestureRecognizer locationInView:self.view];
     
-    CGRect screenBounds = [[UIScreen mainScreen] bounds];
-    CGFloat tappableHeight = screenBounds.size.height - self.view.frame.size.height;
+    CGFloat tappableHeight = 0;
     
+    for (UIView *view in [self.view.subviews[0] subviews]) {
+        if ([view isKindOfClass:[UIImageView class]]) {
+            tappableHeight = view.frame.origin.y;
+        }
+    }
+        
     if (point.y <= tappableHeight) {
         [self dismissViewControllerAnimated:YES completion:nil];
     }
