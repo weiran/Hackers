155f7a3700b6a1b760ff3ed9f79ad917117f23b1
Weiran Zhang <weiran@zhang.me.uk>
2013-04-08 08:02:20 +0100

    Updated pods.

---
+++ Pods/Pods-header.h
@@ -0,0 +1,11 @@
+// WARNING: This feature of CocoaPods is present for discussion purposes and might be discontinued or changed in future
+#define __COCOA_PODS
+
+#define __POD_AFNetworking
+#define __POD_DTCoreText
+#define __POD_DTFoundation_Core
+#define __POD_DTFoundation_DTHMLParser
+#define __POD_GCOLaunchImageTransition
+#define __POD_MBProgressHUD
+#define __POD_OHAttributedLabel
+#define __POD_REMenu
