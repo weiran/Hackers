a8e342c910fcf091301d06d4249aa60a6e6dddcc
Weiran Zhang <weiran@zhang.me.uk>
2013-06-13 11:37:55 +0100

    Updated DTFoundation and cocoapods.

--- Pods/Pods-resources.sh
+++ Pods/Pods-resources.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+RESOURCES_TO_COPY=${PODS_ROOT}/resources-to-copy.txt
+touch "$RESOURCES_TO_COPY"
+
 install_resource()
 {
   case $1 in
@@ -20,8 +23,8 @@ install_resource()
       xcrun momc "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/`basename $1 .xcdatamodeld`.momd"
       ;;
     *)
-      echo "cp -R ${PODS_ROOT}/$1 ${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
-      cp -R "${PODS_ROOT}/$1" "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+      echo "${PODS_ROOT}/$1"
+      echo "${PODS_ROOT}/$1" >> "$RESOURCES_TO_COPY"
       ;;
   esac
 }
@@ -30,3 +33,6 @@ install_resource 'ARChromeActivity/ARChromeActivity/ARChromeActivity@2x.png'
 install_resource 'ARChromeActivity/ARChromeActivity/ARChromeActivity@2x~ipad.png'
 install_resource 'ARChromeActivity/ARChromeActivity/ARChromeActivity~ipad.png'
 install_resource 'DTCoreText/Demo/Resources/DTCoreTextFontOverrides.plist'
+
+rsync -avr --no-relative --exclude '*/.svn/*' --files-from="$RESOURCES_TO_COPY" / "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
+rm "$RESOURCES_TO_COPY"
