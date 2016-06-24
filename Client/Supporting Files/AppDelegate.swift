ab323059531e53ca8b9d0cb799fcd675775804de
Weiran Zhang <weiran@zhang.me.uk>
2016-06-24 08:12:26 +0100

    Download new config file on launch

--- Client/Supporting Files/AppDelegate.swift
+++ Client/Supporting Files/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import libHN
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func applicationDidFinishLaunching(application: UIApplication) {
+        HNManager.sharedManager().startSession()
+        
         UINavigationBar.appearance().titleTextAttributes = [
             NSForegroundColorAttributeName: UIColor.whiteColor()
         ]
