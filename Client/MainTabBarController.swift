7441fa65a475dd82adfe23c0246936cdea0af771
Weiran Zhang <weiran@zhang.me.uk>
2017-09-10 15:27:14 +0100

    Added icons

--- Client/MainTabBarController.swift
+++ Client/MainTabBarController.swift
@@ -17,27 +17,33 @@ class MainTabBarController: UITabBarController {
             if let newsVC = viewController as? NewsViewController {
                 var postType: PostFilterType?
                 var typeName: String?
+                var iconName: String?
                 switch index {
                 case 1:
                     postType = .ask
                     typeName = "Ask"
+                    iconName = "AskIcon"
                     break
                 case 2:
                     postType = .jobs
                     typeName = "Jobs"
+                    iconName = "JobsIcon"
                     break
                 case 3:
                     postType = .new
                     typeName = "New"
+                    iconName = "NewIcon"
                     break
                 case 0:
                     fallthrough
                 default:
                     postType = .top
                     typeName = "Top"
+                    iconName = "TopIcon"
                 }
                 newsVC.postType = postType
                 newsVC.tabBarItem.title = typeName
+                newsVC.tabBarItem.image = UIImage(named: iconName!)
             }
         }
     }
