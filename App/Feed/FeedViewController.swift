bb21aa64ad34b66e0862a4729e10f500542f864b
Weiran Zhang <weiran@zhang.me.uk>
2020-10-23 12:51:39 +0100

    Remove unused code

--- App/Feed/FeedViewController.swift
+++ App/Feed/FeedViewController.swift
@@ -327,36 +327,6 @@ extension FeedViewController: UIViewControllerPreviewingDelegate, SFSafariViewCo
     }
 }
 
-extension FeedViewController { // smoothly deselect cells
-    func setupSmoothlyDeselectRows() {
-        // Get the initially selected index paths, if any
-        let selectedIndexPaths = tableView.indexPathsForSelectedRows ?? []
-
-        // Grab the transition coordinator responsible for the current transition
-        if let coordinator = transitionCoordinator {
-            // Animate alongside the master view controller's view
-            coordinator.animateAlongsideTransition(in: parent?.view, animation: { context in
-                // Deselect the cells, with animations enabled if this is an animated transition
-                selectedIndexPaths.forEach {
-                    self.tableView.deselectRow(at: $0, animated: context.isAnimated)
-                }
-            }, completion: { context in
-                // If the transition was cancel, reselect the rows that were selected before,
-                // so they are still selected the next time the same animation is triggered
-                if context.isCancelled {
-                    selectedIndexPaths.forEach {
-                        self.tableView.selectRow(at: $0, animated: false, scrollPosition: .none)
-                    }
-                }
-            })
-        } else { // If this isn't a transition coordinator, just deselect the rows without animating
-            selectedIndexPaths.forEach {
-                self.tableView.deselectRow(at: $0, animated: false)
-            }
-        }
-    }
-}
-
 extension FeedViewController: Themed {
     func applyTheme(_ theme: AppTheme) {
         view.backgroundColor = theme.backgroundColor
