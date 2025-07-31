111f8a318f96e59d7bbdad5733f7c02acef11165
Weiran Zhang <weiran@zhang.me.uk>
2025-07-31 14:32:45 +0100

    Brute force linking in comments

--- App/SwiftUI/CommentsView.swift
+++ App/SwiftUI/CommentsView.swift
@@ -432,12 +432,86 @@ struct CommentContextMenu: View {
     }
 }
 
-// Simple HTML text view for now - can be enhanced later
 struct HTMLText: View {
     let htmlString: String
+    @State private var attributedText: AttributedString = AttributedString()
 
     var body: some View {
-        Text(htmlString.strippingHTML())
+        Text(attributedText)
+            .onAppear {
+                parseHTMLText()
+            }
+            .onChange(of: htmlString) { _ in
+                parseHTMLText()
+            }
+    }
+
+    private func parseHTMLText() {
+        let processedHTML = htmlString
+            .replacingOccurrences(of: "&amp;", with: "&")
+            .replacingOccurrences(of: "&lt;", with: "<")
+            .replacingOccurrences(of: "&gt;", with: ">")
+            .replacingOccurrences(of: "&quot;", with: "\"")
+            .replacingOccurrences(of: "&#x27;", with: "'")
+            .replacingOccurrences(of: "&#39;", with: "'")
+            .replacingOccurrences(of: "&nbsp;", with: " ")
+
+        // Extract links and create attributed string
+        var result = AttributedString()
+        let linkPattern = "<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1[^>]*?>(.*?)</a>"
+        guard let regex = try? NSRegularExpression(pattern: linkPattern,
+                                                  options: [.caseInsensitive, .dotMatchesLineSeparators]) else {
+            attributedText = AttributedString(processedHTML.strippingHTML())
+            return
+        }
+
+        let nsString = processedHTML as NSString
+        let matches = regex.matches(in: processedHTML, options: [],
+                                    range: NSRange(location: 0, length: nsString.length))
+
+        var lastEnd = 0
+
+        for match in matches {
+            // Add text before the link
+            if match.range.location > lastEnd {
+                let beforeRange = NSRange(location: lastEnd, length: match.range.location - lastEnd)
+                let beforeText = nsString.substring(with: beforeRange).strippingHTML()
+                result += AttributedString(beforeText)
+            }
+
+            // Extract URL and link text
+            let urlRange = match.range(at: 2)
+            let textRange = match.range(at: 3)
+
+            if urlRange.location != NSNotFound && textRange.location != NSNotFound {
+                let urlString = nsString.substring(with: urlRange)
+                let linkText = nsString.substring(with: textRange).strippingHTML()
+
+                var linkAttributedString = AttributedString(linkText)
+                if let url = URL(string: urlString) {
+                    linkAttributedString.link = url
+                    linkAttributedString.foregroundColor = .blue
+                    linkAttributedString.underlineStyle = .single
+                }
+                result += linkAttributedString
+            }
+
+            lastEnd = match.range.location + match.range.length
+        }
+
+        // Add remaining text after last link
+        if lastEnd < nsString.length {
+            let remainingRange = NSRange(location: lastEnd, length: nsString.length - lastEnd)
+            let remainingText = nsString.substring(with: remainingRange).strippingHTML()
+            result += AttributedString(remainingText)
+        }
+
+        // If no links were found, just strip HTML
+        if matches.isEmpty {
+            result = AttributedString(processedHTML.strippingHTML())
+        }
+
+        attributedText = result
     }
 }
 
