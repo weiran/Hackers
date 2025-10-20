22a897155a5a79f99cffbdb0cee25fa94cdb15d5
Weiran Zhang <weiran@zhang.me.uk>
2025-10-20 14:18:04 +0100

    Strengthen upvote accent color

--- DesignSystem/Sources/DesignSystem/Theme/AppColors.swift
+++ DesignSystem/Sources/DesignSystem/Theme/AppColors.swift
@@ -13,7 +13,7 @@ public enum AppColors {
         if UIColor(named: "upvotedColor", in: .main, compatibleWith: nil) != nil {
             return Color("upvotedColor", bundle: .main)
         }
-        return Color.orange
+        return Color(red: 1.0, green: 0.4, blue: 0.0)
     }
 
     public static var appTint: Color {
@@ -35,7 +35,7 @@ public enum AppColors {
         if UIColor(named: "upvotedColor") != nil {
             Color("upvotedColor")
         } else {
-            Color.orange
+            Color(red: 1.0, green: 0.4, blue: 0.0)
         }
     }
 
