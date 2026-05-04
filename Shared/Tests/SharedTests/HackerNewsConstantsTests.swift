c95b803a0a47d8006b3efc97181809f1cd4fd1be
Weiran Zhang <weiran@zhang.me.uk>
2026-05-04 17:00:50 +0100

    Trim redundant non-UI tests

--- Shared/Tests/SharedTests/HackerNewsConstantsTests.swift
+++ Shared/Tests/SharedTests/HackerNewsConstantsTests.swift
@@ -55,39 +55,4 @@ struct HackerNewsConstantsTests {
 
         #expect(itemURL == expectedURL)
     }
-
-    @Test("Constants struct cannot be instantiated")
-    func privateInitializer() {
-        // This test ensures the init is private by attempting to use the struct
-        // The fact that we can access static properties but can't create instances
-        // confirms the design
-
-        // These should work (static access)
-        let baseURL = HackerNewsConstants.baseURL
-        let host = HackerNewsConstants.host
-        let itemPrefix = HackerNewsConstants.itemPrefix
-
-        #expect(baseURL.isEmpty == false)
-        #expect(host.isEmpty == false)
-        #expect(itemPrefix.isEmpty == false)
-
-        // Note: We can't actually test that init() is private in a unit test,
-        // but the compiler would catch any attempt to create an instance
-        // if the init weren't private
-    }
-
-    @Test("Constants are immutable")
-    func constantsImmutability() {
-        // Test that accessing constants multiple times returns the same values
-        let baseURL1 = HackerNewsConstants.baseURL
-        let baseURL2 = HackerNewsConstants.baseURL
-        let host1 = HackerNewsConstants.host
-        let host2 = HackerNewsConstants.host
-        let itemPrefix1 = HackerNewsConstants.itemPrefix
-        let itemPrefix2 = HackerNewsConstants.itemPrefix
-
-        #expect(baseURL1 == baseURL2)
-        #expect(host1 == host2)
-        #expect(itemPrefix1 == itemPrefix2)
-    }
 }
