74aefde31cfbb3a22387d0d2004519890efe8490
Weiran Zhang <weiran@zhang.me.uk>
2026-07-09 09:34:34 +0100

    Revert "Underlap post browser navigation bar"
    This reverts commit 3a0ee672e6f5fe86b6729bf8179eb4c8a3f75811.

--- App/EmbeddedWebView.swift
+++ App/EmbeddedWebView.swift
@@ -23,7 +23,6 @@ final class BrowserController: ObservableObject {
     let webView: WKWebView
     private var navigationDelegate: BrowserNavigationDelegate?
     private var observations: [NSKeyValueObservation] = []
-    private var underlapsTopBar = false
 
     init() {
         let configuration = WKWebViewConfiguration()
@@ -35,7 +34,6 @@ final class BrowserController: ObservableObject {
         self.navigationDelegate = navigationDelegate
         webView.navigationDelegate = navigationDelegate
         installStateObservers()
-        applyTopBarUnderlap(false)
         updateState()
     }
 
@@ -82,13 +80,6 @@ final class BrowserController: ObservableObject {
         let inset = max(bottomInset, 0)
         applyObscuredBottomInset(inset)
         applyScrollViewBottomInset(inset)
-    }
-
-    func applyTopBarUnderlap(_ underlapsTopBar: Bool) {
-        guard self.underlapsTopBar != underlapsTopBar else { return }
-
-        self.underlapsTopBar = underlapsTopBar
-        webView.scrollView.contentInsetAdjustmentBehavior = underlapsTopBar ? .never : .automatic
     }
 
     private func applyObscuredBottomInset(_ inset: CGFloat) {
@@ -198,7 +189,6 @@ struct EmbeddedWebView: View {
     let showsToolbar: Bool
     let bottomWebViewInset: CGFloat
     let obscuredBottomInset: CGFloat
-    let underlapsTopBar: Bool
     @StateObject private var controller: BrowserController
 
     init(
@@ -208,7 +198,6 @@ struct EmbeddedWebView: View {
         showsToolbar: Bool = true,
         bottomWebViewInset: CGFloat = 0,
         obscuredBottomInset: CGFloat = 0,
-        underlapsTopBar: Bool = false,
         controller: BrowserController? = nil
     ) {
         self.url = url
@@ -217,7 +206,6 @@ struct EmbeddedWebView: View {
         self.showsToolbar = showsToolbar
         self.bottomWebViewInset = bottomWebViewInset
         self.obscuredBottomInset = obscuredBottomInset
-        self.underlapsTopBar = underlapsTopBar
         _controller = StateObject(wrappedValue: controller ?? BrowserController())
     }
 
@@ -287,8 +275,7 @@ struct EmbeddedWebView: View {
         BrowserWebView(
             controller: controller,
             url: url,
-            obscuredBottomInset: obscuredBottomInset,
-            underlapsTopBar: underlapsTopBar
+            obscuredBottomInset: obscuredBottomInset
         )
     }
 
@@ -370,7 +357,6 @@ private struct BrowserWebView: UIViewRepresentable {
     let controller: BrowserController
     let url: URL
     let obscuredBottomInset: CGFloat
-    let underlapsTopBar: Bool
 
     func makeCoordinator() -> Coordinator {
         Coordinator()
@@ -378,14 +364,12 @@ private struct BrowserWebView: UIViewRepresentable {
 
     func makeUIView(context: Context) -> WKWebView {
         context.coordinator.requestedURL = url
-        controller.applyTopBarUnderlap(underlapsTopBar)
         controller.applyBottomChromeInset(obscuredBottomInset)
         context.coordinator.scheduleLoad(controller: controller, url: url)
         return controller.webView
     }
 
     func updateUIView(_ webView: WKWebView, context: Context) {
-        controller.applyTopBarUnderlap(underlapsTopBar)
         controller.applyBottomChromeInset(obscuredBottomInset)
         guard context.coordinator.requestedURL != url else { return }
         context.coordinator.requestedURL = url
@@ -456,10 +440,8 @@ struct PostLinkBrowserView: View {
                 showsToolbar: false,
                 bottomWebViewInset: browserBottomInset,
                 obscuredBottomInset: browserObscuredBottomInset,
-                underlapsTopBar: true,
                 controller: browserController
             )
-            .ignoresSafeArea(.container, edges: .top)
 
             if showingCommentsPane {
                 PostCommentsSheet(
@@ -476,7 +458,6 @@ struct PostLinkBrowserView: View {
         .tint(.accentColor)
         .accessibilityIdentifier("browser.view")
         .navigationBarTitleDisplayMode(.inline)
-        .toolbarBackgroundVisibility(.hidden, for: .navigationBar)
         .task {
             guard !showingCommentsPane else { return }
             withAnimation(WebViewAnimations.panel) {
